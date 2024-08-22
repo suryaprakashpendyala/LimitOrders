2d1b2b8a188e003226d624de3fa8b2cdae7d0e22
suryaprakashpendyala <suryaprakashpendyala125@gmail.com>
2024-08-22 22:56:32 +0530

    Update LimitOrderAgent.java

--- limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
+++ limit_orders/src/main/java/org/afob/limit/LimitOrderAgent.java
@@ -6,14 +6,19 @@ import org.afob.prices.PriceListener;
 import java.math.BigDecimal;
 
 public class LimitOrderAgent implements PriceListener {
-
+           private ExceutionClient executionClient;
     public LimitOrderAgent(final ExecutionClient ec) {
+
+        this.ex=executionClient;
     }
 
 
     @Override
     public void priceTick(String productId, BigDecimal price) {
+       if("IBM".euqals(productId)&&price <100.00){
 
+           executionClient.executeOrder(prodctId,1000);
+       }
     }
 
 }
